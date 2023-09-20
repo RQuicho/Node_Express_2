55b8973a688a56b346030916b2897427a00999e8
RQuicho <raymondquicho@gmail.com>
2023-09-19 23:03:09 -0700

    adding numToWord function and adding some tests for the function

--- timeWord.js
+++ timeWord.js
@@ -28,23 +28,56 @@
 //     if (hour.startsWith('0'))
 // }
 
-const timeWord = (time) => {
-    let hour;
-    let minute;
-    let amPm;
+const numToWord = (number) => {
+    let num = +number;
+    // let ones = ['oh one', 'oh two', 'oh three', 'oh four', 'oh five', 'oh six', 'oh seven', 'oh eight', 'oh nine'];
+    let ones = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let tens = ['twenty', 'thirty', 'forty', 'fifty'];
 
-    // handles am or pm
-    if (time.startsWith('0') || time.startsWith('10') || time.startsWith('11')) {
-        amPm = 'am';
-    } else if (time.startsWith('00') || time.startsWith('12') && ) {
-        amPm = '';
+    let numWord;
+
+    if (num > 0 && num < 10) {
+        numWord = ones[num-1];
+    } else if (num >= 10 && num < 20) {
+        numWord = teens[num % 10];
+    } else if (num >= 20 && num < 60) {
+        if (num % 10 === 0) {
+            numWord = tens[Math.floor(num/10) -2];
+        } else {
+            numWord = tens[Math.floor(num/10) -2] + ' ' + ones[(num % 10) - 1];
+        }
     } else {
-        amPm = 'pm';
+        return 'number must be from 0 to 59';
     }
-
-    // converts string number to string word
-
+    return numWord;
 }
 
+// const timeWord = (time) => {
+//     let hour;
+//     let minute;
+//     let time = `${hour}:${minute}`;
+//     let amPm;
 
-module.exports = timeWord;
+//     // handles am or pm
+//     if (+hour < 12 && +hour > -1) {
+//         amPm = 'am';
+//     } else if (+hour >= 12 && +hour < 24) {
+//         amPm = 'pm';
+//     } else if (time === '00:00' || time === "12:00") {
+//         amPm = '';
+//     } else {
+//         amPm = 'incorrect time input';
+//     }
+
+//     // converts string number to string word
+//     // maybe create an array that has 01 = one, 12 = twelve. Like a hard-coded database
+
+
+
+//     // return final string
+//     // `${hour} ${minute} ${amPm}`
+// }
+
+
+module.exports = numToWord;
--- timeWord.test.js
+++ timeWord.test.js
@@ -1,22 +1,38 @@
 const timeWord = require('./timeWord');
+const numToWord = require('./timeWord');
 const request = require('supertest');
+
+describe('#numToWord', () => {
+  test('it is a function', () => {
+    expect(typeof numToWord).toBe('function');
+  });
+  test('converts number to word', () => {
+    expect(numToWord("01")).toEqual("one");
+    expect(numToWord("05")).toEqual("five");
+    expect(numToWord("10")).toEqual("ten");
+    expect(numToWord("13")).toEqual("thirteen");
+    expect(numToWord("20")).toEqual("twenty");
+    expect(numToWord("22")).toEqual("twenty two");
+    expect(numToWord("60")).toEqual("number must be from 0 to 59");
+  });
+});
 
 describe('#timeword', () => {
   test('it is a function', () => {
     expect(typeof timeWord).toBe('function');
   });
-  test("two-digit hour will return word string", () => {
-    expect('00').toEqual('');
-    expect('06').toEqual('six');
-    expect('10').toEqual('ten');
-  });
-  test("two-digit minute will return word string", () => {
-    expect('01').toEqual('oh one');
-    expect('18').toEqual('eighteen');
-  });
-  test("am or pm", () => {
-    expect('00').toEqual('am');
-    expect('10').toEqual('am');
-    epsect('20').toEqual('pm');
-  });
+  // test("two-digit hour will return word string", () => {
+  //   expect('00').toEqual('');
+  //   expect('06').toEqual('six');
+  //   expect('10').toEqual('ten');
+  // });
+  // test("two-digit minute will return word string", () => {
+  //   expect('01').toEqual('oh one');
+  //   expect('18').toEqual('eighteen');
+  // });
+  // test("am or pm", () => {
+  //   expect('00').toEqual('am');
+  //   expect('10').toEqual('am');
+  //   expect('20').toEqual('pm');
+  // });
 });
